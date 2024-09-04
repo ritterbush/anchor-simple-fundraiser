5b853d079a327fce6263134e1e1f7f5fbe3cd70c
ritterbush <ritterbush@berkeley.edu>
2024-09-04 12:16:41 -0700

    feature: new anchor init part 1

--- programs/fundraiser/src/lib.rs
+++ programs/fundraiser/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Eoiuq1dXvHxh6dLx3wh9gj8kSAUpga11krTrbfF5XYsC");
+declare_id!("8ekk3EkAg5o4u3oMWHLDqYXhBA4AZqmQaeecvNDgdWup");
 
 mod state;
 mod instructions;
@@ -12,7 +12,7 @@ use error::*;
 pub use constants::*;
 
 #[program]
-pub mod fundraiser {
+pub mod anchor_simple_fundraiser {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, amount: u64, deadline: i64) -> Result<()> {
